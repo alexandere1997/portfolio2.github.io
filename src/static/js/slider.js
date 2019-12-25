f1c2863d58cc6dd4e7ea0232ffbcac7bee0dde55
Alexander <Sanyi.Ponzel@mail.ru>
2019-12-25 09:18:14 +0300

    портфолио change

--- src/static/js/slider.js
+++ src/static/js/slider.js
@@ -195,4 +195,58 @@ $(document).ready(function(){
       // instead of a settings object
     ]
   });
+  $('.portfolio__slider8').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          adaptiveHeight: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.portfolio__slider9').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="submit" class="prev">&#60;</button>',
+    nextArrow: '<button type="submit" class="next">&gt;</button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          adaptiveHeight: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });
